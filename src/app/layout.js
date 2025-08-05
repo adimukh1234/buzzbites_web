16f32cd6f652d403a0703e8f656c001cfe3f35e0
Aditya Mukherjee <adimukherjee100@gmail.com>
2025-08-05 19:58:05 +0530

    commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import "./globals.css";
 import "./horizontal-scroll-layout.css";
 import SmoothScroll from "../../components/SmoothScroll";
 import FuturisticOverlay from "../../components/FuturisticOverlay";
-import PreLoader from "../../components/PreLoader";
 import FullPageFooter from "../../components/FullPageFooter";
 
 export const metadata = {
@@ -15,9 +14,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body suppressHydrationWarning>
-        {/* Preloader - The suppressHydrationWarning above helps with client/server rendering differences */}
-        <PreLoader />
-        
         {/* Global futuristic background overlays */}
         <FuturisticOverlay opacity="low" className="z-0 fixed" />
         
